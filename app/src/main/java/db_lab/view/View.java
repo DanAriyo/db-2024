3235e48a9842f38e458a81a605a7071b66bf004a
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-03-13 17:04:04 +0100

    modified the View from class to interface

--- app/src/main/java/db_lab/view/View.java
+++ app/src/main/java/db_lab/view/View.java
@@ -1,28 +1,5 @@
 package db_lab.view;
 
-import db_lab.data.dataentity.Product;
-import db_lab.data.dataentity.ProductPreview;
+public interface View {
 
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
-
-public final class View {
-    
 }
