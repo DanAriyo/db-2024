78f12a5265ffe7c07b2c0115f81e411658517a03
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-03-13 16:32:36 +0100

    modified the build settings of the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,12 @@ dependencies {
     implementation("mysql:mysql-connector-java:8.0.29")
     testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation(libs.junit)
+
+    val javafxVersion = "21"
+
+    implementation("org.openjfx:javafx-controls:$javafxVersion")
+    implementation("org.openjfx:javafx-fxml:$javafxVersion")
+
 }
 
 java {
