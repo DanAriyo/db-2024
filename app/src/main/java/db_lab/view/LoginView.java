61bca07f0ba718c793431f8c9bec802a4f1e1a32
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-03-17 16:02:46 +0100

    let the viewimpl and loginview implement the view interface

--- app/src/main/java/db_lab/view/LoginView.java
+++ app/src/main/java/db_lab/view/LoginView.java
@@ -1,5 +1,38 @@
 package db_lab.view;
 
+import java.awt.Button;
+import javafx.fxml.FXML;
+import db_lab.controller.Controller;
+import db_lab.model.Model;
+
 public class LoginView implements View {
+
+    private Controller controller;
+    private Model model;
+
+    private Button LoginButton;
+
+    public LoginView(Controller controller, Model model) {
+        this.controller = controller;
+        this.model = model;
+    }
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'show'");
+    }
+
+    @Override
+    public Controller getController() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getController'");
+    }
+
+    @Override
+    public Model getModel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getModel'");
+    }
 
 }
