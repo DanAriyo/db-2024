3c3eac560bc93229845798bbc088d3fcdce5bd34
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-03-17 18:30:41 +0100

    added some methods to implement

--- app/src/main/java/db_lab/model/Model.java
+++ app/src/main/java/db_lab/model/Model.java
@@ -2,11 +2,21 @@ package db_lab.model;
 
 import db_lab.data.dataentity.Product;
 import db_lab.data.dataentity.ProductPreview;
+import db_lab.data.dataentity.User;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface Model {
-    
+
+    public void newUser(String nome, String cognome, String email,
+            String telefono, String indirizzo, Date data, String cf, String username,
+            String password);
+
+    public List<User> getUsers();
+
+    public boolean checkLogin(String username, String password);
+
 }
