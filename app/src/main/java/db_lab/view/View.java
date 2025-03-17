39186e0458a9cb07e6071cf80362a449fffb0eff
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-03-17 16:01:56 +0100

    added methods to the view interface

--- app/src/main/java/db_lab/view/View.java
+++ app/src/main/java/db_lab/view/View.java
@@ -1,5 +1,14 @@
 package db_lab.view;
 
+import db_lab.controller.Controller;
+import db_lab.model.Model;
+
 public interface View {
+
+    public void show();
+
+    public Controller getController();
+
+    public Model getModel();
 
 }
