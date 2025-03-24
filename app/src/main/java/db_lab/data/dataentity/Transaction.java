6cbf814bb4c5bd3cfe385a02b2e7e36dea048003
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-03-24 15:48:04 +0100

    removed useless libraries

--- app/src/main/java/db_lab/data/dataentity/Transaction.java
+++ app/src/main/java/db_lab/data/dataentity/Transaction.java
@@ -1,11 +1,9 @@
 package db_lab.data.dataentity;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
